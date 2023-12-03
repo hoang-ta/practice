bb137fac26ee09594d529d44d59bab3b9fc69ee4
Hoang Ta <huyhoang.ta97@gmail.com>
2023-12-03 13:45:45 +0700

    fix two sum sorted condition

--- dsa_med_two_sums_sorted.ts
+++ dsa_med_two_sums_sorted.ts
@@ -1,10 +1,11 @@
+//https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/
 function twoSum(nums: number[], target: number): number[] {
   let left = 0;
   let right = nums.length - 1;
-  while (left !== right) {
+  while (left < right) {
     const sum = nums[left] + nums[right];
     if (sum === target) {
-      return [left, right];
+      return [left + 1, right + 1];
     }
     if (sum > target) {
       right--;
@@ -12,5 +13,5 @@ function twoSum(nums: number[], target: number): number[] {
       left++;
     }
   }
-  return [left, right];
+  return [];
 }
