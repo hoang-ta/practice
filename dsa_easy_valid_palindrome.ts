917e471bd9357cf0b189283c0675277ec7241fae
Hoang Ta <huyhoang.ta97@gmail.com>
2023-12-03 12:45:10 +0700

    palidrome

---
+++ dsa_easy_valid_palindrome.ts
@@ -0,0 +1,15 @@
+function isPalindrome(s: string): boolean {
+  const validString = s
+    .toLowerCase()
+    .replace(/[^a-zA-Z0-9]/g, '');
+  let left = 0;
+  let right = validString.length - 1;
+  while (left < right) {
+    if (validString[left] !== validString[right]) {
+      return false;
+    }
+    left++;
+    right--;
+  }
+  return true;
+}
