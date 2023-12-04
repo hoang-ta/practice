cde43753d2d9b71cc0fde0dfc9f4466ea4504494
Hoang Ta <huyhoang.ta97@gmail.com>
2023-12-04 23:09:53 +0700

    better solution

--- dsa-med-longest-substring-without-repeating-characters.ts
+++ dsa-med-longest-substring-without-repeating-characters.ts
@@ -1,34 +1,23 @@
 // https://leetcode.com/problems/longest-substring-without-repeating-characters/
 function lengthOfLongestSubstring(s: string): number {
-  if (s.length === 0) {
-    return 0;
-  }
   let start = 0;
-  let longest = 1;
-  let charIndexList = { [s[start]]: 0 };
-  let end = 1;
+  let longest = 0;
+  let indexTracker = {};
+  let end = 0;
   while (end < s.length) {
-    const appearIndex = charIndexList[s[end]];
-    if (charIndexList[s[end]] === undefined) {
-      charIndexList[s[end]] = end;
-      end++;
-    } else {
-      const stringLength = end - start;
-      longest =
-        longest < stringLength ? stringLength : longest;
-      start = appearIndex + 1;
-      end = start + 1;
-      charIndexList = {
-        [s[start]]: start,
-      };
+    let appearIndex = indexTracker[s[end]];
+    indexTracker[s[end]] = end;
+    if (appearIndex !== undefined) {
+      while (start <= appearIndex) {
+        start++;
+      }
     }
+
+    const stringLength = end - start + 1;
+    longest =
+      longest < stringLength ? stringLength : longest;
+    end++;
   }
-  const stringLength = end - start;
-  longest = longest < stringLength ? stringLength : longest;
+
   return longest;
 }
-
-console.log(lengthOfLongestSubstring('aab'));
-console.log(lengthOfLongestSubstring('aaaa'));
-console.log(lengthOfLongestSubstring('abcbcad'));
-console.log(lengthOfLongestSubstring('abcabcbb'));
