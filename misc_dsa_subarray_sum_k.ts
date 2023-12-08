043f1e64c0774a1f7f1ad572dd8ebcfb63313956
Hoang Ta <huyhoang.ta97@gmail.com>
2023-12-08 07:56:11 +0700

    fix largest subarray sum of k elements

--- misc_dsa_subarray_sum_k.ts
+++ misc_dsa_subarray_sum_k.ts
@@ -7,17 +7,18 @@ function subarraySumFixed(
   }
   let maxSum = 0;
   let sum = 0;
-  let resultStartIndex = 0;
-  for (let i = 0; i < nums.length - 1; i++) {
+  let startIndex = 0;
+  for (let i = 0; i < nums.length; i++) {
     sum += nums[i];
     if (i + 1 < k) continue;
     if (sum > maxSum) {
       maxSum = sum;
-      resultStartIndex = i - 2;
+      startIndex = i - k + 1;
     }
-    sum -= nums[i - 2];
+    sum -= nums[startIndex];
   }
-  return nums.slice(resultStartIndex, resultStartIndex + 3);
+  return nums.slice(startIndex, startIndex + k);
 }
 
 console.log(subarraySumFixed([1, 2, 3, 7, 4, 1], 3));
+console.log(subarraySumFixed([1, 5, 4, 2, 9, 9, 9], 3));
