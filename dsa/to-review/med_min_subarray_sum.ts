de8a28c6343f2b8c3d76774a6c8ccd96ae308418
Hoang Ta <huyhoang.ta97@gmail.com>
2023-12-10 20:48:42 +0700

    refactor

--- dsa/to-review/med_min_subarray_sum.ts
+++ dsa/to-review/med_min_subarray_sum.ts
@@ -3,23 +3,22 @@ function minSubArrayLen(
   target: number,
   nums: number[]
 ): number {
-  let count = 1;
   let minCount = 0;
-  let sum = nums[0];
-  if (sum >= target) return 1;
+  let sum = 0;
   let windowStartIndex = 0;
-  for (let i = 1; i < nums.length; i++) {
+  for (let i = 0; i < nums.length; i++) {
     sum += nums[i];
-    count += 1;
-    while (sum >= target && windowStartIndex <= i) {
+    while (sum >= target) {
       minCount =
-        minCount === 0 ? count : Math.min(minCount, count);
+        minCount === 0
+          ? i - windowStartIndex + 1
+          : Math.min(minCount, i - windowStartIndex + 1);
       sum -= nums[windowStartIndex];
       windowStartIndex += 1;
-      count -= 1;
     }
   }
   return minCount;
 }
+
 console.log(minSubArrayLen(7, [2, 3, 1, 2, 4, 3]));
 console.log(minSubArrayLen(4, [1, 4, 4]));
